2b7226d06b537cb9a32c7db92bedb01c5782b608
Chely Ho <adept_inept@yahoo.com>
2018-02-08 17:13:11 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -7,17 +7,17 @@ def fourth_challenge
       :hero_friends => [
         #your hashes here!
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        :"Benvolio" => {name: "", age: "", attitude: ""}
       ]
    }
 }
 
-  
+
 
 end
