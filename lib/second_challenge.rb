0e20f23581a79091fa7915456cd0f4e1dec65270
Chely Ho <adept_inept@yahoo.com>
2018-02-08 17:06:08 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,11 +4,14 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => {},
-      }, 
+      :hero_friends => []
+      },
     :capulet => {
-      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
       }}
 
-  
+
 end
